a5ff67d427711ddca11a5420f2f1acf96ce4e7c3
Caio Tomazelli <caiotomazelli@google.com>
2023-02-07 18:31:15 +0000

    Removes Drill dependency from API container and cleans up main.py

--- tightlock_api/app/main.py
+++ tightlock_api/app/main.py
@@ -1,38 +1,30 @@
 """Entrypoint for FastAPI application."""
 import contextlib
-from datetime import datetime
+import datetime
 import json
-import os
 
 from db import get_session
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import Depends
+from fastapi import FastAPI
+from fastapi import HTTPException
 from fastapi.responses import Response
 import httpx
-from models import Activation, Config
-from pydrill.client import PyDrill
+from models import Activation
+from models import Config
+import requests
 from sqlalchemy.exc import IntegrityError
 from sqlmodel import select
 from sqlmodel.ext.asyncio.session import AsyncSession
-import requests
 
+# Creates base app and v1 API objects
 app = FastAPI()
 v1 = FastAPI()
-
-drill = PyDrill(host=os.environ.get("DRILL_HOSTNAME"),
-                port=os.environ.get("DRILL_PORT"))
-
-if not drill.is_active():
-  raise Exception("Please run Drill first")
-
-# TODO(b/265713009): Update drill connections each time a new Config is created
-
-
-async def update_drill_connections(conns):
-  pass
+_AIRFLOW_BASE_URL = "http://airflow-webserver:8080"
 
 
 @app.on_event("startup")
 async def create_initial_config():
+  """Initializes the system with an initial config when it is not found."""
   f = open("base_config.json")
   data = json.load(f)
   get_session_wrapper = contextlib.asynccontextmanager(get_session)
@@ -45,22 +37,20 @@ async def create_initial_config():
       pass  # Ignore workers trying to recreate initial config
 
 
-@app.on_event("shutdown")
-async def shutdown():
-  pass
-
-
 # TODO(b/264568702)
 @v1.post("/connect")
-def connect():
+async def connect():
+  """Validates API connection with a client."""
   pass
 
 
 @v1.post("/activations/{activation_name}:trigger")
 async def trigger_activation(activation_name: str):
-  url = f"http://airflow-webserver:8080/api/v1/dags/{activation_name}_dag/dagRuns"
+  """Trigger an activation identified by name."""
+  url = f"{_AIRFLOW_BASE_URL}/api/v1/dags/{activation_name}_dag/dagRuns"
+  now_date = str(datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")) 
   body = {
-      "logical_date": str(datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')),
+      "logical_date": now_date,
       "conf": {},
   }
   async with httpx.AsyncClient() as client:
@@ -84,6 +74,7 @@ async def get_configs(session: AsyncSession = Depends(get_session)):
 
 @v1.get("/configs:getLatest", response_model=Config)
 async def get_latest_config(session: AsyncSession = Depends(get_session)):
+  """Retrieves the most recent config."""
   statement = select(Config).order_by(Config.create_date.desc()).limit(1)
   result = await session.execute(statement)
   row = result.one()
@@ -93,8 +84,9 @@ async def get_latest_config(session: AsyncSession = Depends(get_session)):
 
 
 @v1.get("/configs/{config_id}", response_model=Config)
-async def get_config(config_id: int, session: AsyncSession = Depends(get_session)):
-  # description: get a config with the provided id
+async def get_config(config_id: int,
+                     session: AsyncSession = Depends(get_session)):
+  """Retrieves a config with the provided id."""
   statement = select(Config).where(Config.id == config_id)
   config = await session.execute(statement)
   return Config(id=config.id,
@@ -106,6 +98,7 @@ async def get_config(config_id: int, session: AsyncSession = Depends(get_session
 @v1.post("/configs", response_model=Config)
 async def create_config(config: Config,
                         session: AsyncSession = Depends(get_session)):
+  """Creates a new config using the provided config object."""
   session.add(config)
   try:
     await session.commit()
@@ -113,20 +106,16 @@ async def create_config(config: Config,
     raise HTTPException(status_code=409,
                         detail=f"Config label {config.label} already exists."
                         ) from exc
-
-  update_drill_connections(config.value["external_connections"])
   return config
 
 
-# TODO(b/264569406)
 @v1.get("/activations", response_model=list[Activation])
 async def get_activations(session: AsyncSession = Depends(get_session)):
-  # description: query latest config and query activations field from config json
+  """Query latest config and query activations field from config json."""
   latest_config = await get_latest_config(session=session)
-  return [Activation(name=activation["name"],
-                     source_name=activation["source_name"],
-                     destination_name=activation["destination_name"],
-                     schedule=activation["schedule"])
-          for activation in latest_config.value["activations"]]
+  return [
+      Activation(name=a["name"], source_name=a["source_name"],
+                 destination_name=a["destination_name"], schedule=a["schedule"])
+      for a in latest_config.value["activations"]]
 
 app.mount("/api/v1", v1)
