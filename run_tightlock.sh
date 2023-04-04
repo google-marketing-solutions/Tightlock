ee515f2d24f7c60dfb8e327bc7af1119ad4ae18e
Caio Tomazelli <caiotomazelli@google.com>
2023-04-04 19:00:47 +0000

    Add API Key generation prompt to run_tightlock.sh

--- run_tightlock.sh
+++ run_tightlock.sh
@@ -1,2 +1,18 @@
-echo -e "AIRFLOW_UID=$(id -u)\nAIRFLOW_GID=0" > .env
+ENV=".env"
+if ! [ -f $ENV ]; then
+  # create env file and write Airflow UID ang GID to it
+  echo -e "AIRFLOW_UID=$(id -u)\nAIRFLOW_GID=0" > $ENV
+
+  # generate or read API key
+  echo "Choose API key generation method."
+  select yn in "User-provided" "Pseudorandom"; do
+      case $yn in
+          User-provided ) read -p "Enter API KEY: " API_KEY; break;;
+          Pseudorandom ) API_KEY=$( cat /dev/urandom | tr -dc '[:alpha:]' | fold -w ${1:-20} | head -n 1 ); echo "API key: ${API_KEY}"; break;;
+      esac
+  done
+  # append API key to env file
+  echo -e "TIGHTLOCK_API_KEY=$API_KEY" >> $ENV
+fi
+
 docker-compose up --build -d
--- tightlock_api/app/security.py
+++ tightlock_api/app/security.py
@@ -1,8 +1,8 @@
+import os
+
 from fastapi import Depends, HTTPException
 from fastapi.security import APIKeyHeader
 from starlette import status
-import os
-import logging
 
 # Security checks
 X_API_KEY = APIKeyHeader(name="X-API-Key")
@@ -11,9 +11,8 @@ TIGHTLOCK_API_KEY = os.environ.get("TIGHTLOCK_API_KEY")
 
 def check_authentication_header(x_api_key: str = Depends(X_API_KEY)):
   """Takes the X-API-Key header and validates it."""
-  lf x_api_key != TIGHTLOCK_API_KEY:
+  if x_api_key != TIGHTLOCK_API_KEY:
     raise HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Invalid API Key",
     )
-
