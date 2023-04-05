39717e5c063662df18cd340d070198f99a4758af
Caio Tomazelli <caiotomazelli@google.com>
2023-04-05 00:03:52 +0000

    Add --non-interactive fllag to un script.

--- run_tightlock.sh
+++ run_tightlock.sh
@@ -4,13 +4,18 @@ if ! [ -f $ENV ]; then
   echo -e "AIRFLOW_UID=$(id -u)\nAIRFLOW_GID=0" > $ENV
 
   # generate or read API key
-  echo "Choose API key generation method."
-  select yn in "User-provided" "Pseudorandom"; do
-      case $yn in
-          User-provided ) read -p "Enter API KEY: " API_KEY; break;;
-          Pseudorandom ) API_KEY=$( cat /dev/urandom | tr -dc '[:alpha:]' | fold -w ${1:-20} | head -n 1 ); echo "API key: ${API_KEY}"; break;;
-      esac
-  done
+  PSEUDORANDOM_API_KEY=$( cat /dev/urandom | tr -dc '[:alpha:]' | fold -w20 | head -n 1 )
+  if ! [ $1 == "--non-interactive" ]; then
+    echo "Choose API key generation method."
+    select yn in "User-provided" "Pseudorandom"; do
+        case $yn in
+            User-provided ) read -p "Enter API KEY: " API_KEY; break;;
+            Pseudorandom ) API_KEY=$PSEUDORANDOM_API_KEY; echo "API key: ${API_KEY}"; break;;
+        esac
+    done
+  else
+    API_KEY=$PSEUDORANDOM_API_KEY
+  fi
   # append API key to env file
   echo -e "TIGHTLOCK_API_KEY=$API_KEY" >> $ENV
 fi
