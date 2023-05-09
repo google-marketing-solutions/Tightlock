962625c2ea8c324cf7c52c676c9c2d3a5f9a3f42
Caio Tomazelli <caiotomazelli@google.com>
2023-05-09 01:08:45 +0000

    Adding initial provisioning for installer

---
+++ installer/main.tf
@@ -0,0 +1,25 @@
+resource "google_compute_address" "vm-static-ip" {
+  name    = "vm-static-ip"
+  project = "tightlock-dev-gke"
+  region  = "us-central1"
+}
+
+resource "google_compute_instance" "tightlock-backend" {
+  name         = "tightlock-backend"
+  machine_type = "e2-standard-4"
+  zone         = "us-central1-a"
+  project = "tightlock-dev-gke"
+
+ boot_disk {
+    initialize_params {
+      image = "cos-cloud/cos-105-17412-1-75"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      nat_ip = "${google_compute_address.vm-static-ip.address}"
+    }
+  }
+}
