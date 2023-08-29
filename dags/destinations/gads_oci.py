32bbf7808c8c13f891f346ee034bb5367a32d964
graham.mcrobbie <gmcrobbie@google.com>
2023-08-29 20:01:41 +0000

    Update logic to get valid and invalid conversions

--- dags/destinations/gads_oci.py
+++ dags/destinations/gads_oci.py
@@ -21,22 +21,30 @@ import datetime
 import enum
 import json
 import logging
+from collections import defaultdict
 from typing import Any, Dict, List, Mapping, Optional, Sequence, Tuple
 
 import errors
 import immutabledict
 import requests
-import utils
 from pydantic import Field
 from utils import DagUtils, ProtocolSchema, RunResult, SchemaUtils, ValidationResult
 
 _BATCH_SIZE = 10000
+
+_DEFAULT_CURRENCY_CODE = "USD"
 
 _REQUIRED_FIELDS = [
   "customer_id",
   "conversion_action_id",
   "conversion_date_time",
   "conversion_value",
+]
+
+_ID_FIELDS = [
+  "gclid",
+  "gbraid",
+  "wbraid",
 ]
 
 
@@ -50,7 +58,7 @@ class Destination:
       config: Configuration object to hold environment variables
     """
     self._config = config  # Keeping a reference for convenience.
-    self.google_ads_client = DagUtils().build_google_ads_client(self._config)
+    self._client = DagUtils().build_google_ads_client(self._config)
     self._debug = config.get("debug", False)
 
     print("Initialized Google Ads OCI Destination class.")
@@ -58,10 +66,19 @@ class Destination:
   def send_data(
       self, input_data: List[Mapping[str, Any]], dry_run: bool
   ) -> Optional[RunResult]:
-    """Builds payload ans sends data to Google Ads API."""
-    valid_events, invalid_indices_and_errors = self._get_valid_and_invalid_events(
+    """Builds payload and sends data to Google Ads API.
+
+    Args:
+      input_data: A list of rows to send to the API endpoint.
+
+    Returns: A RunResult summarizing success / failures, etc.
+    """
+    valid_conversions, invalid_indices_and_errors = self._get_valid_and_invalid_conversions(
         input_data
     )
+
+    # TODO: Batch here? Or will Tightlock do batching to this method?
+    # TODO: How to handle partial failures?
 
     if not dry_run:
       for valid_event in valid_events:
@@ -97,6 +114,88 @@ class Destination:
 
     return run_result
 
+  def _get_valid_and_invalid_conversions(
+      self, offline_conversions: List[Dict[str, Any]]
+  ) -> Dict[str, List[Tuple[int, Any]]], List[Tuple[int, errors.ErrorNameIDMap]]]:
+    """Prepares the offline conversion data for API upload.
+
+    Args:
+      offline_conversions: The offline conversion data to upload..
+
+    Returns:
+      A dictionary of customer IDs mapped to index-conversion tuples for the
+      valid conversions, and a list of index-error for the invalid conversions.
+
+    For example:
+      valid_conversions = {"customer1": [(1, "ConversionOne"), (2, "Conversion2")]}
+      invalid_indices_and_errors = [3, MISSING_MADATORY_FIELDS, ...]
+    """
+    valid_conversions = defaultdict(list)
+    invalid_indices_and_errors = []
+
+    for i, conversion in enumerate(offline_conversions):
+      valid = True
+
+      # Checks required fields set.
+      for required_field in _REQUIRED_FIELDS:
+        if not conversion.get(required_field, ""):
+          invalid_indices_and_errors.append((i, errors.ErrorNameIDMap.ADS_OC_HOOK_ERROR_MISSING_MANDATORY_FIELDS))
+          valid = False
+
+      # Checks exactly one of glid, gbraid, or braid is set.
+      target_ids = [
+        conversion.get(key, "") for key in _ID_FIELDS if conversion.get(key, "")]
+
+      if len(target_ids) != 1:
+        invalid_indices_and_errors.append((i, errors.ErrorNameIDMap.ADS_OC_HOOK_ERROR_MISSING_MANDATORY_FIELDS))
+        valid = False
+
+      if not valid:
+        # Invalid conversion.
+        continue
+
+      # Builds the API click payload.
+      click_conversion = self._client.get_type("ClickConversion")
+      conversion_upload_service = self._client.get_service("ConversionUploadService")
+      conversion_action_service = self._client.get_service("ConversionActionService")
+
+      customer_id = conversion.get("customer_id")
+
+      click_conversion.conversion_action = conversion_action_service.conversion_action_path(
+        customer_id, conversion.get("conversion_action_id", "")
+      )
+
+      gclid = conversion.get("gclid", "")
+      gbraid = conversion.get("gbraid", "")
+      wbraid = conversion.get("wbraid", "")
+
+      # Sets the single specified ID field.
+      if gclid:
+        click_conversion.gclid = gclid
+      elif gbraid:
+        click_conversion.gbraid = gbraid
+      else:
+        click_conversion.wbraid = wbraid
+
+      click_conversion.conversion_value = float(conversion.get("conversion_value", ""))
+      click_conversion.conversion_date_time = conversion.get("conversion_date_time", "")
+      click_conversion.currency_code = conversion.get("conversion_date_time", _DEFAULT_CURRENCY_CODE)
+
+      conversion_customer_variable_id = conversion.get("conversion_custom_variable_id", "")
+      conversion_custom_variable_value = conversion.get("conversion_custom_variable_value", "")
+
+      if conversion_custom_variable_id and conversion_custom_variable_value:
+        conversion_custom_variable = self._client.get_type("CustomVariable")
+        conversion_custom_variable.conversion_custom_variable = conversion_upload_service.conversion_custom_variable_path(
+          customer_id, conversion_custom_variable_id
+        )
+        conversion_custom_variable.value = conversion_custom_variable_value
+        click_conversion.custom_variables.append(conversion_custom_variable)
+
+      valid_conversions[customer_id].append((i, click_conversion))
+
+    return valid_conversions, invalid_indices_and_errors
+
   @staticmethod
   def schema() -> Optional[ProtocolSchema]:
     """Returns the required metadata for this destination config.
@@ -120,6 +219,9 @@ class Destination:
         ("conversion_custom_variable_id", Optional[str], Field(default=None,
         description=("The ID of the conversion custom variable to associate "
         "with the upload."))),
+        ("currency_code", Optional[str], Field(
+          default=_DEFAULT_CURRENCY_CODE, description=("The currency code "
+          "for the value of this conversion."))),
         ("conversion_custom_variable_value", Optional[str], Field(
           default=None, description=("The value of the conversion custom "
           "variable to associate with the upload."))),
