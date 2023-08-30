d5bf53168dc2a6d15f2a6b43c1040e4c2a9bbc53
graham.mcrobbie <gmcrobbie@google.com>
2023-08-30 20:41:41 +0000

    Update oci destination

--- dags/destinations/gads_oci.py
+++ dags/destinations/gads_oci.py
@@ -14,13 +14,12 @@ See the License for the specific language governing permissions and
 limitations under the License."""
 
 """Google Ads OCI destination implementation."""
-
-import logging
-from collections import defaultdict
-from typing import Any, Dict, List, Mapping, Optional, Sequence, Tuple
-
 import errors
+
+from collections import defaultdict
+from google.ads.googleads.errors import GoogleAdsException
 from pydantic import Field
+from typing import Any, Dict, List, Mapping, Optional, Sequence, Tuple
 from utils import DagUtils, ProtocolSchema, RunResult, ValidationResult
 
 _BATCH_SIZE = 10000
@@ -43,6 +42,7 @@ _ID_FIELDS = [
 ConversionIndicesToConversions = List[Tuple[int, Any]]
 CustomerConversionMap = Dict[str, ConversionIndicesToConversions]
 InvalidConversionIndices = List[Tuple[int, errors.ErrorNameIDMap]]
+PartialFailures = Dict[int, str]
 
 
 class Destination:
@@ -74,42 +74,53 @@ class Destination:
     Returns: A RunResult summarizing success / failures, etc.
     """
     valid_conversions, invalid_indices_and_errors = self._get_valid_and_invalid_conversions(
-        input_data
+      input_data
     )
+    successfully_uploaded_conversions = []
 
     # TODO: Batch here? Or will Tightlock do batching to this method?
     if not dry_run:
       for customer_id, conversion_data in valid_conversions.items():
+        conversion_indices = [data[0] for data in conversion_data]
+        conversions = [data[1] for data in conversion_data]
+
         try:
-          # TODO: Conversion data is a list of conversions for this customer ID
-          indices = [data[0] for data in conversion_data]
-          conversions = [data[1] for data in conversion_data]
-          self._send_request(customer_id, conversions)
-        except (
-            errors.DataOutConnectorSendUnsuccessfulError,
-            errors.DataOutConnectorValueError,
-        ) as error:
-          index = indices[0]  # TODO: Extract and set correct error indices
-          invalid_indices_and_errors.append((index, error.error_num))
+          partial_failures = self._send_request(customer_id, conversions)
+        except GoogleAdsException as error:
+          # Set every index as failed
+          err_msg = error.error.code().name
+          invalid_indices_and_errors.extend([(index, err_msg) for index in conversion_indices])
+        else:
+          # Handles partial failures: Checks which conversions were successfully
+          # sent, and which failed.
+          partial_failure_indices = set(partial_failures.keys())
+
+          for index in range(len(conversions)):
+            # Maps index from this customer's conversions back to original input data index.
+            original_index = conversion_indices[index]
+            if index in partial_failure_indices:
+              invalid_indices_and_errors.append((original_index, partial_failures[index]))
+            else:
+              successfully_uploaded_conversions.append(original_index)
     else:
       print(
-          "Dry-Run: Events will not be sent to the API."
+        "Dry-Run: Events will not be sent to the API."
       )
 
-    print(f"Valid conversions: {valid_conversions}")
+    print(f"Sent conversions: {successfully_uploaded_conversions}")
     print(f"Invalid events: {invalid_indices_and_errors}")
 
     for invalid_conversion in invalid_indices_and_errors:
       conversion_index = invalid_conversion[0]
-      error_num = invalid_conversion[1]
+      error = invalid_conversion[1]
       # TODO(b/272258038): TBD What to do with invalid events data.
-      print(f"conversion_index: {conversion_index}; error_num: {error_num}")
+      print(f"conversion_index: {conversion_index}; error: {error}")
 
     return RunResult(
-        successful_hits=len(valid_events),
-        failed_hits=len(invalid_indices_and_errors),
-        error_messages=[str(error[1]) for error in invalid_indices_and_errors],
-        dry_run=dry_run,
+      successful_hits=len(successfully_uploaded_conversions),
+      failed_hits=len(invalid_indices_and_errors),
+      error_messages=[str(error[1]) for error in invalid_indices_and_errors],
+      dry_run=dry_run,
     )
 
   def _get_valid_and_invalid_conversions(
@@ -140,7 +151,7 @@ class Destination:
           invalid_indices_and_errors.append((i, errors.ErrorNameIDMap.ADS_OC_HOOK_ERROR_MISSING_MANDATORY_FIELDS))
           valid = False
 
-      # Checks exactly one of glid, gbraid, or braid is set.
+      # Checks exactly one of gclid, gbraid, or wbraid is set.
       target_ids = [
         conversion.get(key, "") for key in _ID_FIELDS if conversion.get(key, "")]
 
@@ -154,7 +165,6 @@ class Destination:
 
       # Builds the API click payload.
       click_conversion = self._client.get_type("ClickConversion")
-      conversion_upload_service = self._client.get_service("ConversionUploadService")
       conversion_action_service = self._client.get_service("ConversionActionService")
 
       customer_id = conversion.get("customer_id")
@@ -195,25 +205,72 @@ class Destination:
 
     return valid_conversions, invalid_indices_and_errors
 
-  def _send_request(self, customer_id: str, conversions: List[Any]) -> None:
+  def _send_request(self, customer_id: str, conversions: List[Any]) -> PartialFailures:
     """Sends conversions to the offline conversion import API.
 
     Args:
       customer_id: The customer ID for these conversions.
       conversions: A list of click conversions.
+
+    Returns: An empty dict if no partial failures exist, or a dict of the index
+      mapped to the error message.
     """
     request = self._client.get_type("UploadClickConversionsRequest")
     request.customer_id = customer_id
     request.conversions.extend(conversions)
     request.partial_failure = True
-    conversion_upload_response = self._conversion_upload_service.upload_click_conversions(
-      request=request,
-    )
+    try:
+      conversion_upload_response = self._conversion_upload_service.upload_click_conversions(
+        request=request,
+      )
+    except GoogleAdsException as error:
+      print(f"Caught GoogleAdsException: {error}")
+      raise
 
     for i, result in enumerate(conversion_upload_response.results):
       print(f"Conversion {i}: {result}")
-    # TODO: Check and handle errors.
-    # TODO: How to handle partial failures?
+
+    return self._get_partial_failures(conversion_upload_response)
+
+  def _get_partial_failures(self, response: Any) -> PartialFailures:
+    """Checks whether a response message has a partial failure error.
+
+    In Python the partial_failure_error attr is always present on a response
+    message and is represented by a google.rpc.Status message. So we can't
+    simply check whether the field is present, we must check that the code is
+    non-zero. Error codes are represented by the google.rpc.Code proto Enum:
+    https://github.com/googleapis/googleapis/blob/master/google/rpc/code.proto
+
+    Args:
+        response:  A MutateAdGroupsResponse message instance.
+
+    Returns: An empty dict if no partial failures exist, or a dict of the index
+      index mapped to the error message.
+    """
+    partial_failure = getattr(response, "partial_failure_error", None)
+    code = getattr(partial_failure, "code", None)
+    if code == 0:
+      # No failures.
+      return {}
+
+    error_details = getattr(partial_failure, "details", [])
+
+    partial_failures = defaultdict(str)
+
+    for error_detail in error_details:
+      # Retrieve an instance of the GoogleAdsFailure class from the client
+      failure_message = self._client.get_type("GoogleAdsFailure")
+      # Parse the string into a GoogleAdsFailure message instance.
+      # To access class-only methods on the message we retrieve its type.
+      GoogleAdsFailure = type(failure_message)
+      failure_object = GoogleAdsFailure.deserialize(error_detail.value)
+
+      for error in failure_object.errors:
+        index = error.location.field_path_elements[0].index
+        message = f'Code: {error.error_code}, Error: {error.message}'
+        partial_failures[index] += message  # Can be multiple errors for the same conversion.
+
+    return partial_failures
 
   @staticmethod
   def schema() -> Optional[ProtocolSchema]:
@@ -235,31 +292,34 @@ class Destination:
           "time). The format is 'yyyy-mm-dd hh:mm:ss+|-hh:mm, e.g. "
           "'2019-01-01 12:32:45-08:00'"))),
         ("conversion_value", str, Field(description="The conversion value.")),
-        ("conversion_custom_variable_id", Optional[str], Field(default=None,
-        description=("The ID of the conversion custom variable to associate "
-        "with the upload."))),
+        ("conversion_custom_variable_id", Optional[str], Field(
+          default=None, description=("The ID of the conversion custom variable to associate "
+                                     "with the upload."))),
         ("currency_code", Optional[str], Field(
           default=_DEFAULT_CURRENCY_CODE, description=("The currency code "
-          "for the value of this conversion."))),
+                                                       "for the value of this conversion."))),
         ("conversion_custom_variable_value", Optional[str], Field(
           default=None, description=("The value of the conversion custom "
-          "variable to associate with the upload."))),
-        ("gclid", Optional[str], default=None, Field(description=(
+                                     "variable to associate with the upload."))),
+        ("gclid", Optional[str], Field(description=(
           "The Google Click Identifier (gclid) which should be newer than "
           "the number of days set on the conversion window of the conversion "
           "action. Only one of either a gclid, WBRAID, or GBRAID identifier can "
           "be passed into this example. See the following for more details: "
-          "https://developers.google.com/google-ads/api/docs/conversions/upload-clicks"))),
-        ("gbraid", Optional[str], default=None, Field(description=(
+          "https://developers.google.com/google-ads/api/docs/conversions/upload-clicks"),
+          default=None)),
+        ("gbraid", Optional[str], Field(description=(
           "The GBRAID identifier for an iOS app conversion. Only one of "
           "either a gclid, WBRAID, or GBRAID identifier can be passed into this "
           "example. See the following for more details: "
-          "https://developers.google.com/google-ads/api/docs/conversions/upload-clicks"))),
-        ("wbraid", Optional[str], Field(default=None, description=(
+          "https://developers.google.com/google-ads/api/docs/conversions/upload-clicks"),
+          default=None)),
+        ("wbraid", Optional[str], Field(description=(
           "The WBRAID identifier for an iOS app conversion. Only one of "
           "either a gclid, WBRAID, or GBRAID identifier can be passed into this "
           "example. See the following for more details: "
-          "https://developers.google.com/google-ads/api/docs/conversions/upload-clicks"))),
+          "https://developers.google.com/google-ads/api/docs/conversions/upload-clicks"),
+          default=None, )),
       ]
     )
 
