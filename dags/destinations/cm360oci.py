fa665ad1d66885c5b916fad5beee8874863b4d6e
Djimbaye Andemel <djimbaye@google.com>
2023-10-26 01:01:00 +0000

    fixed auth issues

--- dags/destinations/cm360oci.py
+++ dags/destinations/cm360oci.py
@@ -25,21 +25,13 @@ from typing import Any, Dict, List, Mapping, Optional, Sequence, Tuple
 
 from googleapiclient import discovery
 import httplib2
-from oauth2client import client
-from oauth2client import tools
-from oauth2client.file import Storage
+import google.oauth2.credentials
 
 import errors
 import immutabledict
 import requests
 from pydantic import Field
 from utils import ProtocolSchema, RunResult, SchemaUtils, ValidationResult
-
-# Filename used for the credential store.
-CREDENTIAL_STORE_FILE = 'auth-file.dat'
-
-# The OAuth 2.0 scopes to request.
-OAUTH_SCOPES = ['https://www.googleapis.com/auth/dfareporting']
 
 CM_CONVERSIONS_FIELDS = [
   "floodlightConfigurationId",
@@ -80,65 +72,66 @@ CM_MUTUALLY_EXCLUSIVE_CONVERSIONS_FIELDS = [
   "impressionId",
 ]
 
+CM_CREDENTIALS = [
+  "access_token",
+  "refresh_token",
+  "token_uri",
+  "client_id",
+  "client_secret",
+]
+
 class Destination:
   """Implements DestinationProto protocol for Campaign Manager Offline Conversion Import."""
 
   def __init__(self, config: Dict[str, Any]):
     self.config = config  # Keeping a reference for convenience.
     self.profileId = config.get("profile_id")
-    self.client_secrets_file = config.get("client_secrets_file")
-    self.client_secrets = config.get("secrets")
-    f = open(self.client_secrets_file, "w")
+    #self.client_secrets_file = config.get("client_secrets_file")
+    self.credentials = {}
+    for credential in CM_CREDENTIALS:
+      self.credentials[credential] = config.get(credential,"")
+      
+    '''f = open(self.client_secrets_file, "w")
     f.write(json.dumps(self.client_secrets))
-    f.close()
-    # Authenticate using the supplied user account credentials
-    self.http = self.authenticate_using_user_account(self.client_secrets_file)
-    self.validate()
-    self._validate_credentials()
+    f.close()'''
+    
     self.encryption_info = {}
     self.encryption_info["encryptionEntityType"] = config.get("encryptionEntityType","")
     self.encryption_info["encryptionEntityId"] = config.get("encryptionEntityId","")
     self.encryption_info["encryptionEntitySource"] = config.get("encryptionEntitySource","")
     self.encryption_info["kind"] = config.get("kind","")
+    self.validate()
+    self._validate_credentials()
+    # Authenticate using the supplied user account credentials
+    self.http = self.authenticate_using_user_account()
 
-  def authenticate_using_user_account(self,client_secrets_file):
+  def authenticate_using_user_account(self):
     """Authorizes an httplib2.Http instance using user account credentials."""
-    # Set up a Flow object to be used if we need to authenticate.
-    flow = client.flow_from_clientsecrets(
-        client_secrets_file, scope=OAUTH_SCOPES)
+    
+    credentials = google.oauth2.credentials.Credentials(
+    self.credentials['access_token'],
+    refresh_token = self.credentials['refresh_token'],
+    token_uri = self.credentials['token_uri'],
+    client_id = self.credentials['client_id'],
+    client_secret = self.credentials['client_secret'])
 
-    # Check whether credentials exist in the credential store. Using a credential
-    # store allows auth credentials to be cached, so they survive multiple runs
-    # of the application. This avoids prompting the user for authorization every
-    # time the access token expires, by remembering the refresh token.
-    storage = Storage(CREDENTIAL_STORE_FILE)
-    #credentials = storage.get()
-
-    # If no credentials were found, go through the authorization process and
-    # persist credentials to the credential store.
-    #if credentials is None or credentials.invalid:
-    credentials = tools.run_flow(flow, storage)
-
-    # Use the credentials to authorize an httplib2.Http instance.
+    '''# Use the credentials to authorize an httplib2.Http instance.
     http = credentials.authorize(httplib2.Http())
 
-    return http
-  
+    return http'''
+    return True
+
   def _validate_credentials(self) -> None:
     """Validate credentials.
 
     Raises:
       Exception: If credential combination does not meet criteria.
     """
-    if not self.client_secrets_file:
-      raise errors.DataOutConnectorValueError(
-          f"Missing client secrets file in config: {self.config}"
-      )
-
-    if not self.client_secrets:
-      raise errors.DataOutConnectorValueError(
-          f"Missing client secrets in config: {self.config}"
-      )
+    for credential in CM_CREDENTIALS:
+      if not self.credentials[credential]:
+        raise errors.DataOutConnectorValueError(
+            f"Missing {credential} in config: {self.config}"
+        )
 
   def _parse_timestamp_micros(self, conversion: Dict[str, Any]):
     t = conversion.get("timestamp_micros")
@@ -165,7 +158,7 @@ class Destination:
       debug flag.
     """
 
-    authenticate_using_user_account(self.path_to_client_secrets_file)
+    #authenticate_using_user_account(self.path_to_client_secrets_file)
 
     # Construct a service object via the discovery service.
     service = discovery.build('dfareporting', 'v4', http=http)
@@ -235,17 +228,19 @@ class Destination:
         dry_run=dry_run,
     )
 
-    return True
-
   @staticmethod
   def schema() -> Optional[ProtocolSchema]:
     return ProtocolSchema(
-        "CM360OCI",
-        [
-          ("client_id", str, Field(description="An OAuth2.0 Web Client ID.")),
-          ("client_secret", str, Field(description="An OAuth2.0 Web Client Secret.")),
-          ("profile_id", str, Field(description="A Camaign Manager Profile ID .")),
-        ]
+      "CM360OCI",
+      [
+        ("profile_id", str, Field(description="A Camaign Manager Profile ID .")),
+        ("client_secrets_file", str, Field(description="Client secrets file.")),
+        ("encryptionEntityType", str, Field(description="The encryption entity type.")),
+        ("encryptionEntityId", str, Field(description="The encryption entity ID.")),
+        ("encryptionSource", str, Field(description="Describes whether the encrypted cookie was received from ad serving or from Data Transfer.")),
+        ("kind", str, Field(description="Identifies what kind of resource this is.")),
+        ("secrets", str, Field(description="JSON CM360 credentials including client id and client secret.")),
+      ]
     )
 
   def fields(self) -> Sequence[str]:
@@ -262,15 +257,16 @@ class Destination:
     """
     missing_encryption_fields = []
 
-    for encryption_field in self.encryptionInfo:
-      if not self.encryptionInfo[encryption_field]:
-        missing_encryption_fields.append(encryption_fields)
+    for encryption_field in self.encryption_info:
+      if not self.encryption_info[encryption_field]:
+        missing_encryption_fields.append(encryption_field)
 
     if missing_encryption_fields:
       error_msg = (
         "Config requires the following fields to be set: "
         f"{', '.join(missing_encryption_fields)}")
       return ValidationResult(False, [error_msg])
+    return ValidationResult(True, [error_msg])
 
   def validate_payload(self, payload) -> ValidationResult:
     """Validates the conversions list.
