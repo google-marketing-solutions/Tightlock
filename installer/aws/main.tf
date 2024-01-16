5830c8e8d80b77253ac33474c04c6127eed89d4d
Caio Tomazelli <caiotomazelli@google.com>
2024-01-16 21:13:45 +0000

    Fixes cloud-config / first working draft

--- installer/aws/main.tf
+++ installer/aws/main.tf
@@ -23,31 +23,24 @@ resource "random_string" "backend_name" {
 }
 
 # Network resources
-resource "aws_vpc" "tightlock-vpc" {
-  count = var.create_tightlock_network ? 1 : 0
-  cidr_block = "10.0.0.0/16"  # Adjust CIDR block as needed
-  tags = {
-    Name = "tightlock-vpc"
-  }
-}
+# resource "aws_vpc" "tightlock-vpc" {
+#   cidr_block = "10.0.0.0/16"  # Adjust CIDR block as needed
+#   tags = {
+#     Name = "tightlock-vpc"
+#   }
+# }
 
-resource "aws_subnet" "tightlock-subnet" {
-  count = var.create_tightlock_network ? 1 : 0
-  vpc_id     = aws_vpc.tightlock-vpc[0].id
-  cidr_block = "10.0.1.0/24"  # Adjust CIDR block as needed
-  tags = {
-    Name = "tightlock-subnet"
-  }
-}
-
-resource "aws_key_pair" "dev_key" {
-  key_name = "dev-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCrbbjF6stjIZBkRBaxo4F988Rz+pauvp+BysJqyZKyFCmNT4oF1k5NNtHgL4T3ynyVevggnH15jdctmef1wJHhMR9dsvqjTcPe+mpUfY/nFrRzx2L1YVkhzoyXEzgfUs1gx8ds+FNLYoAPVDbwuYwBe1v4YWahrxXPkWJ3FDgZ54wgVFiVeQ3A9IcWfp4WTM5EGhMsbpibPo1eBdwrA5I6jzRHLM8ry+g8ie3HeJCwzukjmJMjcdqUSkhZUrUKI610aUIRXqFHHPI3jG7CCFDUfyUHHCarwmGi7mNiibSb1nqE7FIMCRfCR3EaX7WO7WhCaln0tT15g7ULpGwv0p6KkLf1re/KDQRB0ELs77o51pYljQvQfqojN2xgWqNsXsiqrbBx59XGYkttKp1c8cuQ8bKgln9638ErYSJ2kziMFHtf9z/y5xj5JfV916cYWSbCnsK+ttvX/QlWwkCwZB4XegrdK8je7EgDBpofhRv0lRb5gT/WoEAT8a9HWS833bk= cloudshell-user@ip-10-134-90-139.us-east-2.compute.internal"
-}
+# resource "aws_subnet" "tightlock-subnet" {
+#   vpc_id     = aws_vpc.tightlock-vpc.id
+#   cidr_block = "10.0.0.0/16"  # Adjust CIDR block as needed
+#   tags = {
+#     Name = "tightlock-subnet"
+#   }
+# }
 
 resource "aws_security_group" "tightlock-security-group" {
   name   = "tightlock-sg"
-    # vpc_id = aws_vpc.tightlock-vpc[0].id
+  # vpc_id = aws_vpc.tightlock-vpc.id
 
   ingress {
     protocol    = "tcp"
@@ -68,6 +61,13 @@ resource "aws_security_group" "tightlock-security-group" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress{
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -94,12 +94,12 @@ resource "aws_instance" "tightlock-backend" {
   ami           = "ami-0c2f3d2ee24929520"  # Replace with a suitable Amazon Linux 2 AMI
   availability_zone = var.aws_availability_zone
   associate_public_ip_address = true
-  # instance_type = "t3.medium"  # Map to equivalent AWS instance type
-  instance_type = "t2.micro"  # Map to equivalent AWS instance type
-  key_name = "ssh-test"
+  instance_type = "t2.large"  # Map to equivalent AWS instance type
+  # instance_type = "t2.micro"  # Map to equivalent AWS instance type
+  key_name = "tightlock"
   user_data = templatefile("cloud-config.yaml", { API_KEY = "${var.api_key}"})
   vpc_security_group_ids = [aws_security_group.tightlock-security-group.id]
-  # subnet_id              = aws_subnet.tightlock-subnet[0].id
+  # subnet_id              = aws_subnet.tightlock-subnet.id
   tags = {
     Name = format("tightlock-backend-%s", random_string.backend_name.result)
   }
@@ -109,10 +109,7 @@ resource aws_volume_attachment "tightlock-storage-att" {
   device_name = "/dev/sdt"
   volume_id = aws_ebs_volume.tightlock-storage.id
   instance_id = aws_instance.tightlock-backend.id
-}
-
-output "Test" {
-  value = aws_instance.tightlock-backend
+  stop_instance_before_detaching = true
 }
 
 output "Connection_Code" {
