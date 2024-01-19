4c08a10a62cfe395e38a77c70dea014a7e5b34a0
Caio Tomazelli <caiotomazelli@google.com>
2024-01-19 18:00:23 +0000

    Final adjustments for AWS base installer

--- installer/aws/main.tf
+++ installer/aws/main.tf
@@ -1,4 +1,3 @@
-# Provider block for AWS
 terraform {
   required_providers {
     aws = {
@@ -14,7 +13,6 @@ provider "aws" {
   secret_key = var.secret_key
 }
 
-# Equivalent for random_string
 resource "random_string" "backend_name" {
   length  = 4
   special = false
@@ -22,53 +20,14 @@ resource "random_string" "backend_name" {
   upper   = false
 }
 
-# Network resources
-# resource "aws_vpc" "tightlock-vpc" {
-#   cidr_block = "10.0.0.0/16"  # Adjust CIDR block as needed
-#   tags = {
-#     Name = "tightlock-vpc"
-#   }
-# }
-
-# resource "aws_subnet" "tightlock-subnet" {
-#   vpc_id     = aws_vpc.tightlock-vpc.id
-#   cidr_block = "10.0.0.0/16"  # Adjust CIDR block as needed
-#   tags = {
-#     Name = "tightlock-subnet"
-#   }
-# }
-
 resource "aws_security_group" "tightlock-security-group" {
   name   = "tightlock-sg"
-  # vpc_id = aws_vpc.tightlock-vpc.id
 
   ingress {
     protocol    = "tcp"
     from_port   = 80
     to_port     = 80
-    cidr_blocks = ["0.0.0.0/0"]
-    #cidr_blocks = ["35.199.32.68/32"]  # Allow connections from 1pd-scheduler.dev
-  }
-
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress{
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["35.199.32.68/32"]  # Allow connections from 1pd-scheduler.dev
   }
 
   egress {
@@ -82,7 +41,7 @@ resource "aws_security_group" "tightlock-security-group" {
 # Storage resources
 resource "aws_ebs_volume" "tightlock-storage" {
   availability_zone = var.aws_availability_zone
-  size              = 20
+  size              = 50
   type              = "gp2"  # Change type if needed
   tags = {
     Name = format("tightlock-%s-storage", random_string.backend_name.result)
@@ -91,15 +50,13 @@ resource "aws_ebs_volume" "tightlock-storage" {
 
 # Instance resources
 resource "aws_instance" "tightlock-backend" {
-  ami           = "ami-0c2f3d2ee24929520"  # Replace with a suitable Amazon Linux 2 AMI
+  ami           = "ami-0c2f3d2ee24929520"  
   availability_zone = var.aws_availability_zone
   associate_public_ip_address = true
-  instance_type = "t2.large"  # Map to equivalent AWS instance type
-  # instance_type = "t2.micro"  # Map to equivalent AWS instance type
+  instance_type = "t2.large"  
   key_name = "tightlock"
   user_data = templatefile("cloud-config.yaml", { API_KEY = "${var.api_key}"})
   vpc_security_group_ids = [aws_security_group.tightlock-security-group.id]
-  # subnet_id              = aws_subnet.tightlock-subnet.id
   tags = {
     Name = format("tightlock-backend-%s", random_string.backend_name.result)
   }
