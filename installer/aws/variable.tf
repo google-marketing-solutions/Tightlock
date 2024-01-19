0437ffe5ec0f8763ca7c343c779f8edc82b8ff19
Caio Tomazelli <caiotomazelli@google.com>
2024-01-19 18:05:41 +0000

    Fixes merge details

--- installer/aws/variable.tf
+++ installer/aws/variable.tf
@@ -14,11 +14,6 @@ variable "secret_key" {
   description = "AWS Secret Key"
 }
 
-variable "create_tightlock_network" {
-  type        = bool
-  description = "If set to true, the default Tightlock network and firewall will be created. Set it to false if deploying on a cloud project that already has a Tightlock deployment."
-}
-
 variable "aws_availability_zone" {
   type        = string
   description = "The zone that the machine should be created in."
